5874f2358b64fad6963c2417b59c64bfb59085ea
Morgan <morgan.lombard@protonmail.com>
2022-06-04 21:26:07 +0200

    fix nullable env keycloak settings

--- src/Keycloak.js
+++ src/Keycloak.js
@@ -1,9 +1,9 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: window._env_.KC_URL ?? 'http://localhost:8080/auth',
-  realm: window._env_.KC_REALM ?? 'adventureforge-api',
-  clientId: window._env_.KC_CLIENTID ?? 'adventureforge-ui',
+  url: window?._env_?.KC_URL ?? 'http://localhost:8080/auth',
+  realm: window?._env_?.KC_REALM ?? 'adventureforge-api',
+  clientId: window?._env_?.KC_CLIENTID ?? 'adventureforge-ui',
 });
 
 export default keycloak;
