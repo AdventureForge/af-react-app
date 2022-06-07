d8612f15836ac6532a5800b388a9580c170e2d16
Morgan <morgan.lombard@protonmail.com>
2022-06-07 03:36:37 +0200

    disable alert

--- src/pages/UserSpace.tsx
+++ src/pages/UserSpace.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useCallback, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import SideNavBar from '../components/layout/SideNavBar';
